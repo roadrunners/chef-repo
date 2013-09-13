d2c62db171c6149e24612c8eef475b4c4e5badae
Karan Misra <karan@erodov.com>
2013-09-14 04:17:18 +0530

    mysql.rb: tune the mysql server configuration

--- roles/mysql.rb
+++ roles/mysql.rb
@@ -1,6 +1,21 @@
 name "mysql"
 description "MySQL"
 default_attributes({
+  mysql: {
+    tunable: {
+      key_buffer_size: "32M",
+      "myisam-recover" => "FORCE,BACKUP",
+
+      max_connections: 500,
+      thread_cache_size: 50,
+      table_open_cache: 2048,
+
+      innodb_flush_method: "O_DIRECT",
+      innodb_flush_log_at_trx_commit: 2,
+      innodb_file_per_table: 1,
+      innodb_buffer_pool_size: "3G"
+    }
+  },
   ulimit: {
     users: {
       nginx: {
